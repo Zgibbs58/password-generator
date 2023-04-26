c9940ee2d4f4d4f946b02d33560769b395a03a99
Zgibbs58 <zacharywgibbs@gmail.com>
2023-04-25 21:32:30 -0500

    added a characters prompt that will specify how many characters should be in the password. Put that value in the for loop.

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-function generatePassword(length) {
-  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
-  var password = ""
-  for (let i = 0; i < length; i++) {
-    const randCharset = charset.;
-    
-  }
-}
-
-
 
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword(length) {
+  //all code goes here look at alerts, prompts 
+  let characters = prompt("How many characters?")
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+  var password = ""
+  for (let i = 0; i < characters; i++) {
+    var randCharset = charset.charAt(Math.floor(Math.random() * charset.length));
+    password += randCharset
+  }
+  return password;
+}
+
+console.log(generatePassword(10));
 
 // Write password to the #password input
 function writePassword() {
@@ -24,3 +28,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// Hint strings are iterable
+// look up prompts for javascript
